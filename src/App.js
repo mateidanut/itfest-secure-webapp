30ab012fc54fc32ef7556fcf11996176fd66343c
Danut Matei <matei.danut.dm@gmail.com>
2020-11-07 12:06:40 +0200

    First ITFest commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, {Component} from "react";
-import LoginButton from "./LoginButton";
+import React, { Component } from 'react';
+import LoginButton from './LoginButton';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <LoginButton/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <p>Login here:</p>
+        <LoginButton />
+      </div>
+    );
+  }
 }
 
 export default App;
